8d9bba8be0cf90461f0e1dd741a85a62e52c8e02
vermanil <anilrajverma1996@gmail.com>
2016-11-11 19:47:37 +0530

    add table in schema table

--- model/book_author_schema.js
+++ model/book_author_schema.js
@@ -20,4 +20,48 @@ book_author1.save(function(err, book_author){
 
 });
 
+var book_author2 = new book_author({
+    isbn : 9781449325947,
+    author : 'DAVID SAWYER'
+});
+
+book_author2.save(function(err, book_author){
+    if (err) return console.error(err);
+    console.log('book_author table created');
+
+});
+
+var book_author3 = new book_author({
+    isbn : 9781449363260,
+    author : 'Matthew MacDonald'
+});
+
+book_author3.save(function(err, book_author){
+    if (err) return console.error(err);
+    console.log('book_author table created');
+
+});
+
+var book_author4 = new book_author({
+    isbn : 9788178087368,
+    author: 'BRIAN W.KERNIGHAN'
+});
+
+book_author4.save(function(err, book_author){
+    if (err) return console.error(err);
+    console.log('book_author table created');
+
+});
+
+var book_author5 = new book_author({
+    isbn : 9780471694205,
+    author : 'AMOS GILAT'
+});
+
+book_author5.save(function(err, book_author){
+    if (err) return console.error(err);
+    console.log('book_author table created');
+
+});
+
 module.exports = book_author;
--- model/booksden_book_schema.js
+++ model/booksden_book_schema.js
@@ -34,4 +34,81 @@ booksden1.save(function(err, booksden)
 
 });
 
+
+var booksden2 = new booksden({
+    isbn : 9781449325947,
+    title : 'CSS3: MISSING MANUAL',
+    publisher : 'MISSING MANUAL',
+    p_year : 2016-02-06,
+    edition : 3,
+    lang : 'English',
+    pages : 650,
+    edited_by : 102
+});
+
+booksden2.save(function(err, booksden)
+{
+    if (err) return console.error('err')
+    console.log('booksden table created')
+
+});
+
+
+var booksden3 = new booksden({
+    isbn : 9781449363260,
+    title : 'HTML:5',
+    publisher : 'MISSING MANUAL',
+    p_year : 2016-02-06,
+    edition : 2,
+    lang : 'English',
+    pages : 519,
+    edited_by : 101
+
+});
+
+booksden3.save(function(err, booksden)
+{
+    if (err) return console.error('err')
+    console.log('booksden table created')
+
+});
+
+
+var booksden4 = new booksden({
+    isbn : 9788178087368,
+    title : 'The C Programming Language',
+    publisher : 'Prentice HALL',
+    p_year : 2016-02-06,
+    edition : 2,
+    lang : 'English',
+    pages : 457,
+    edited_by : 101
+});
+
+booksden4.save(function(err, booksden)
+{
+    if (err) return console.error('err')
+    console.log('booksden table created')
+
+});
+
+
+var booksden5 = new booksden({
+    isbn : 9780471694205,
+    title : 'MATLAB:AN INTRODUCTION WITH AP',
+    publisher : 'WILEY',
+    p_year : 2016-02-06,
+    edition : 5,
+    lang : 'English',
+    pages : 325,
+    edited_by : 102
+});
+
+booksden5.save(function(err, booksden)
+{
+    if (err) return console.error('err')
+    console.log('booksden table created')
+
+});
+
 module.exports = booksden;
