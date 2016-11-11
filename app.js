0f5ae4054640f1875b54cc42429faf7e84185d54
ansarimofid <ansarimofid@gmail.com>
2016-11-11 17:49:56 +0530

    Added date formatting

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var hbs= require('hbs');
+var hbs = require('hbs');
 var db = require('./model/started');
 
 var index = require('./controllers/index');
@@ -25,7 +25,7 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -35,14 +35,14 @@ app.use('/test', test);
 app.use('/books', books);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -54,5 +54,11 @@ app.use(function(err, req, res, next) {
 
 // hbs partial registration
 hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('formatDate', function (date) {
+  var months = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  date = new Date(date);
+  console.log(date);
+  return months[date.getMonth()+1]+', '+date.getFullYear() ;
+});
 
 module.exports = app;
