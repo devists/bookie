ef32dc123272c2a655b76b0de6c31f3fcb0d4899
ansarimofid <ansarimofid@gmail.com>
2016-11-13 13:18:07 +0530

    Adds sessions support to app

--- controllers/users.js
+++ controllers/users.js
@@ -2,48 +2,70 @@ var express = require('express');
 var router = express.Router();
 //var bodyParser = require('body-parser');
 var student = require('../model/student_schema');
+var session = require('express-session');
+
+var expressOptions = {
+  secret: "secret",
+  saveUninitialized:false,
+  resave:true
+};
+
+router.use(session(expressOptions));
+
+router.get('/test',function (req,res) {
+
+  res.send(req.session);
+
+});
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
+  res.send('respond with a resource');
 });
 
 router.get('/login', function (req, res, next) {
-    res.render('login', {})
+  res.render('login', {})
+});
+
+router.get('/logout',function (req,res) {
+  req.session.destroy(function (err) {
+    if (err)
+      res.send(err);
+
+    res.send("Logout Successfully");
+  })
 });
 
 router.post('/login', function (req, res, next) {
-    console.log(req.body);
-    student.findOne({username: req.body.username}, function(err, logindata) {
-        if (err)
-            console.log(err);
-        else if (req.body.username === logindata.username  &&  req.body.password === logindata.password) {
-            res.send('successfully login');
-            console.log("successfully login");
-            //console.log(logindata.password);
-        }
-        else
-            console.log('invalid username or password');
-            //console.log(logindata.password);
-    });
-    //res.render('login', {})
+  student.findOne({username: req.body.username}, function (err, loginData) {
+    if (err)
+      console.log(err);
+    else if (req.body.username === loginData.username && req.body.password === loginData.password) {
+      req.session.userData = loginData;
+      console.log(loginData);
+      res.send('successfully login');
+      console.log("successfully login");
+    }
+    else
+      console.log('invalid username or password');
+  });
 });
 
 router.get('/register', function (req, res, next) {
-    res.render('register', {})
+  res.render('register', {})
 });
 
-router.post('/', function (req, res, next){
-    res.send('successfully register');
-    console.log(req.body);
-    var newStudent = student(req.body);
-    newStudent.save(function (err, newtable) {
-        if (err)
-            console.log(err)
-        else
-            res.render('login', {})
-        //console.log("successfully register");
-    });
+router.post('/', function (req, res, next) {
+  res.send('successfully register');
+  console.log(req.body);
+  var newStudent = student(req.body);
+  newStudent.save(function (err, newtable) {
+    if (err)
+      console.log(err);
+    else
+      res.render('login', {});
+    //console.log("successfully register");
+  });
 });
 
 module.exports = router;
